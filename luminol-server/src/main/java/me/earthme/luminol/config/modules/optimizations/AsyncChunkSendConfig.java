3e7291f52d9fb21bd73247d20570fea73877af77
Tuanvo29110 <tuananh290011@gmail.com>
2025-09-10 07:18:37 +0000

    Async chunk sending

---
+++ luminol-server/src/main/java/me/earthme/luminol/config/modules/optimizations/AsyncChunkSendConfig.java
@@ -0,0 +1,12 @@
+package me.earthme.luminol.config.modules.optimizations;
+
+import me.earthme.luminol.config.IConfigModule;
+import me.earthme.luminol.config.flags.ConfigClassInfo;
+import me.earthme.luminol.config.flags.ConfigInfo;
+import me.earthme.luminol.enums.EnumConfigCategory;
+
+@ConfigClassInfo(configAttribution = EnumConfigCategory.OPTIMIZATIONS, mainName = "use_async_chunk_send")
+public class AsyncChunkSendConfig implements IConfigModule {
+    @ConfigInfo(baseName = "enabled")
+    public static boolean enabled = false;
+}
